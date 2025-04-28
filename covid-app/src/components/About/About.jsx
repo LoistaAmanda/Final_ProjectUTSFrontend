27ec6f98103f0d882f0f1a12e391854dbc77ca60
LoistaAmanda <luluamandel@gmail.com>
2025-04-28 16:48:50 +0700

    push hasil project UTS Pemrograman Frontend

--- covid-app/src/components/About/About.jsx
+++ covid-app/src/components/About/About.jsx
@@ -4,29 +4,29 @@ import image from "../../assets/image/image1.png"
 function About(){
     return (
       <div className={styles.container}>
-        <section className={styles.covid}>
-          <div className={styles.covid__left}>
-            <h1 className={styles.covid__title}>Covid ID</h1>
-            <h3 className={styles.covid__information}>
-              Monitoring Perkembangan Covid
+        <section className={styles.about}>
+          <div className={styles.about__left}>
+            <h1 className={styles.about__title}>Covid ID</h1>
+            <h3 className={styles.about__information}>
+              Monitoring Perkembangan about
             </h3>
-            <p className={styles.covid__description}>
+            <p className={styles.about__description}>
               Website Covid ID merupakan platform resmi yang dibuat pemerintah
               Indonesia untuk memantau dan menyajikan perkembangan terkini
-              seputar COVID-19 di Indonesia. Melalui situs ini, masyarakat dapat
+              seputar Covid-19 di Indonesia. Melalui situs ini, masyarakat dapat
               mengakses data terbaru mengenai jumlah kasus, penyebaran,
               informasi kesehatan, serta berbagai kebijakan dan protokol
               penanganan pandemi yang berlaku di tingkat nasional maupun daerah.
               Website ini juga berfungsi sebagai sumber informasi terpercaya
               untuk menghindari hoaks, serta menyediakan layanan edukasi dan
               mentoring bagi masyarakat dan tenaga kesehatan terkait penanganan
-              dan pencegahan COVID-19
+              dan pencegahan Covid-19
             </p>
-            <button className={styles.covid__button}>Vaccine</button>
+            <button className={styles.about__button}>Vaccine</button>
           </div>
-          <div className={styles.covid__right}>
+          <div className={styles.about__right}>
             <img
-              className={styles.covid__image}
+              className={styles.about__image}
               src={image}
               alt="Medical workers with medicine"
             />
